8dad685715329c5854220a6ec70dbd9d3dc8fe26
Noah Diewald <noah.diewald@gmail.com>
2010-08-30 21:39:09 -0500

    added more descriptive configuration title

--- src/config_resource.erl
+++ src/config_resource.erl
@@ -5,6 +5,7 @@
 -module(config_resource).
 -export([
   init/1, 
+  resource_exists/2,
   to_html/2
 ]).
 
@@ -14,5 +15,19 @@
 init([]) -> {ok, undefined}.
 
 to_html(ReqData, State) ->
-    {ok, Html} = config_dtl:render([{title, "Configuration"},{project, wrq:path_info(project, ReqData)}]),
-    {Html, ReqData, State}.
+  Id = wrq:path_info(project, ReqData) -- "project-",
+  ProjectUrl = ?ADMINDB ++ "projects/" ++ Id,
+  
+  {ok, "200", _, JsonIn} = ibrowse:send_req(ProjectUrl, [], get),
+  {struct, Json} = mochijson2:decode(JsonIn),
+  
+  {ok, Html} = config_dtl:render([{title, "Configuration"}, {project, wrq:path_info(project, ReqData)}, {project_info, Json}]),
+  {Html, ReqData, State}.
+
+resource_exists(ReqData, State) ->
+  DatabaseUrl = ?ADMINDB ++ wrq:path_info(project, ReqData),
+  
+  case ibrowse:send_req(DatabaseUrl, [], head) of
+    {ok, "200", _, _} -> {true, ReqData, State};
+    {ok, "404", _, _} -> {false, ReqData, State}
+  end. 
