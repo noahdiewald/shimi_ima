c5cbccca1aa0321b8693ec48d075a35688bc52d3
Noah Diewald <noah.diewald@gmail.com>
2010-09-21 22:37:48 -0500

    working tests for icu

--- test/icu_test.erl
+++ test/icu_test.erl
@@ -15,18 +15,18 @@ get_sort_key_test_() ->
   % Equivalent binaries should get equivalent keys
   ?_assertEqual(icu:get_sort_key(<<"h">>), icu:get_sort_key(<<"h">>)),
   
-  % Detect bad encoding in string
+  % Detect bad argument type
   ?_assertError(badarg, icu:get_sort_key("ha͞ew", "hey")),
   
-  % Ensure that a reasonable locale works
+  % Ensure that a locale works
   ?_assertMatch({ok, _}, icu:get_sort_key("en_US", <<"hey">>)),
   
-  % Equivalent binaries should get equivalent keys
+  % Equivalent binaries should get equivalent keys with locales too.
   ?_assertEqual(icu:get_sort_key("en_US", <<"h">>), icu:get_sort_key("en_US", <<"h">>)),
   
-  % The keys must always be the same of a locale
+  % The keys must always be the same
   ?_assertEqual({ok, <<58,1,5,1,5,0>>}, icu:get_sort_key("en_US", <<"h">>)),
   
-  % Blank is ok
+  % Blank locale is ok
   ?_assertMatch({ok, _}, icu:get_sort_key("", <<"h">>))
 ].
