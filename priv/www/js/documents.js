5cede427311f5f4ed07be68c6aa8e3f4c69036f5
Noah Diewald <noah.diewald@gmail.com>
2010-11-03 02:18:07 -0500

    needed to get validations working

--- priv/www/js/documents.js
+++ priv/www/js/documents.js
@@ -434,6 +434,9 @@ function fieldsToObject(fieldsContainer, fieldsIndex) {
       head: field.attr('data-field-head') == "true",
       reversal: field.attr('data-field-reversal') == "true",
       required: field.attr('data-field-required') == "true",
+      min: field.attr('data-field-min'),
+      max: field.attr('data-field-max'),
+      regex: field.attr('data-field-regex'),
       order: field.attr('data-field-order') * 1,
       subcategory: field.attr('data-field-subcategory'),
       value: fieldValue
