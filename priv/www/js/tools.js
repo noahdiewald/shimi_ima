d149c5ca61b88ae3e53983bc1ca7bbacffaed334
Noah Diewald <noah.diewald@gmail.com>
2011-03-11 18:05:52 -0600

    adding tools section

---
+++ priv/www/js/tools.js
@@ -0,0 +1,43 @@
+function initTabs() {
+  $("#main-tabs").tabs();
+  //populateDoctypeTabs();
+  
+  return false;
+}
+
+function initHelpText() {
+  $("#doctype-info").hide();
+  $("#charseq-info").hide();
+
+  $("#doctype-info-toggle").click(function() {
+    $("#doctype-info").toggle("blind", {}, 500);
+    return false;
+  });
+  
+  $("#charseq-info-toggle").click(function() {
+    $("#charseq-info").toggle("blind", {}, 500);
+    return false;
+  });
+  
+  return false;
+}
+
+$(function () {
+  initTabs(); 
+  initHelpText();
+  //getIndex();
+  //initEdit();
+  
+  //$('#index-filter-form input').keyup(function() {
+  //  getIndex();
+  //});
+  
+  //$('#index-filter-form select').change(function() {
+  //  getIndex();
+  //});
+  
+  $('#all-query-container').accordion({ 
+    collapsible: true,
+    autoHeight: false
+  });
+});
---
+++ src/tools_resource.erl
@@ -0,0 +1,67 @@
+%% @author Noah Diewald <noah@diewald.me>
+%% @copyright 2010 University of Wisconsin Madison Board of Regents.
+%% Copyright (c) 2010 University of Wisconsin Madison Board of Regents
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining
+%% a copy of this software and associated documentation files (the
+%% "Software"), to deal in the Software without restriction, including
+%% without limitation the rights to use, copy, modify, merge, publish,
+%% distribute, sublicense, and/or sell copies of the Software, and to
+%% permit persons to whom the Software is furnished to do so, subject to
+%% the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included
+%% in all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+%% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+%% NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+%% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+%% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+%% THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%% @doc Currently focused on simply rendering the configuration page.
+
+-module(tools_resource).
+-export([
+  init/1,
+  is_authorized/2, 
+  resource_exists/2,
+  to_html/2,
+  validate_authentication/3
+]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+-include_lib("include/config.hrl").
+
+init(Opts) -> {ok, Opts}.
+
+to_html(R, S) ->
+  User = proplists:get_value(user, S),
+  Project = couch:get_json(project, R, S),
+  
+  Vals = [{<<"user">>, User},{<<"project_info">>, Project}],
+  
+  {ok, Html} = tools_dtl:render(Vals),
+  {Html, R, S}.
+
+resource_exists(R, S) ->
+  DatabaseUrl = ?ADMINDB ++ wrq:path_info(project, R),
+  
+  case ibrowse:send_req(DatabaseUrl, [], head) of
+    {ok, "200", _, _} -> {true, R, S};
+    {ok, "404", _, _} -> {false, R, S}
+  end. 
+
+is_authorized(R, S) ->
+  proxy_auth:is_authorized(R, [{source_mod, ?MODULE}|S]).
+
+validate_authentication(Props, R, S) ->
+  Project = couch:get_json(project, R, S),
+  Name = jsn:get_value(<<"name">>, Project),
+  ValidRoles = [<<"_admin">>, <<"manager">>, Name],
+  IsMember = fun (Role) -> lists:member(Role, ValidRoles) end,
+  case lists:any(IsMember, proplists:get_value(<<"roles">>, Props)) of
+    true -> {true, R, S};
+    false -> {proplists:get_value(auth_head, S), R, S}
+  end.
