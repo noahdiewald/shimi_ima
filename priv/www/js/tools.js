beb895e6036749e291011b2c69e8671a23f09281
Noah Diewald <noah.diewald@gmail.com>
2011-03-12 03:52:39 -0600

    creating front end for query manipulation

--- priv/www/js/tools.js
+++ priv/www/js/tools.js
@@ -22,10 +22,63 @@ function initHelpText() {
   return false;
 }
 
+function initQueryNewDialog() {
+  var queryOwner = $("#query-owner-input");
+  var queryDoctype = $("#query-doctype-input");
+  var queryName = $("#query-name-input");
+  
+  var dialog = $("#query-new-dialog").dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+      "Create": function() {
+        $('.input').removeClass('ui-state-error');
+        
+        // place holder for client side validation
+        checkResult = true;
+        
+        if (checkResult) {
+          var obj = {
+            "category": "query", 
+            "owner": queryOwner.val(), 
+            "name": queryName.val(), 
+            "doctype": queryDoctype.val()
+          },
+          complete = function(context) {
+            //populateDoctypeTabs();
+            $(context).dialog("close");
+          };
+          sendConfigDoc("queries", obj, 'POST', complete, this);
+        }
+      },
+      "Cancel": function() {
+        $(this).dialog("close");
+      }
+    },
+    close: function() {
+      clearValues($('.input')).removeClass('ui-state-error');
+    }
+  });
+  
+  return dialog;
+}
+
+function initQueryNewButton() {
+  $('#new-query-button').button({
+    icons: {primary: "ui-icon-plus"}
+  }).click(function() {
+    initQueryNewDialog().dialog("open");
+  });
+  
+  return false;
+}
+
 $(function () {
   initTabs(); 
   initHelpText();
   $('#query-builder-dialog').hide();
+  $('#query-new-dialog').hide();
+  initQueryNewButton();
   
   //getIndex();
   //initEdit();
