81069d4e4dd2e6df9f935a548e0b71febde92822
Noah Diewald <noah.diewald@gmail.com>
2011-03-16 23:31:26 -0500

    first steps toward saving template #523

--- src/query_resource.erl
+++ src/query_resource.erl
@@ -133,7 +133,10 @@ json_update(R, S) ->
   Json2 = jsn:set_value(<<"_rev">>, list_to_binary(Rev), Json1),
   
   case couch:update(doc, Id, jsn:encode(Json2), R, S) of
-    {ok, updated} -> {true, R, S};
+    {ok, updated} -> 
+      Expression = conditions:trans(jsn:get_value(<<"conditions">>, Json)),
+      {ok, Rendered} = design_query_json_dtl:render([{<<"expression">>, Expression}|Json2]),
+      {true, R, S};
     {403, Message} ->
       R1 = wrq:set_resp_body(Message, R),
       {{halt, 403}, R1, S};
