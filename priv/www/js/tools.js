0a4412a01676430dada88f332599e469d8b6048c
Noah Diewald <noah.diewald@gmail.com>
2011-04-09 22:10:42 -0500

    adding comment

--- priv/www/js/tools.js
+++ priv/www/js/tools.js
@@ -31,6 +31,12 @@ function setQueryDoctypeEvents(queryDoctype, queryFieldset) {
   return false;
 }
 
+// Add events to a fieldset. In this case a change event that will populate
+// the select options of a following field input element.  queryDoctype may
+// be either a string corresponding to the doctype or an input field with
+// the doctype as a value. The queryFieldset and queryField should both
+// be select elements.
+
 function setQueryFieldsetEvents(queryDoctype, queryFieldset, queryField) {
   queryFieldset.change(function() {
     if (!(typeof queryDoctype == "string")) {
