616442f54611c5cf580b0cad0d0ddc35f35a48b1
Noah Diewald <noah@diewald.me>
2011-10-03 02:38:11 -0500

    remembering semicolons

--- priv/www/js/config/click-targets.js
+++ priv/www/js/config/click-targets.js
@@ -2,13 +2,13 @@
 
 var addCharsec = function(button) {
   $("#charseq-add-dialog").dialog("open");
-}
+};
 
 // Button that opens a dialog for adding a doctype
 
 var addDoctype = function(target) {
   initDoctypeAddDialog().dialog("open");
-}
+};
 
 // Button that opens a dialog for editing a field
 
@@ -22,7 +22,7 @@ var editFieldButton = function(target) {
   });
   
   fieldDialog(url, oldobj).dialog("open");
-}
+};
 
 // Button that opens a dialog for deleting a field
 
@@ -39,14 +39,14 @@ var deleteFieldButton = function(target) {
     };
     url.delete(complete, this);
   }
-}
+};
 
 // Button that opens a dialog for adding a field
 
 var addFieldButton = function(target) {
   var url = cpath(target, "field");
   fieldDialog(url, {fieldset: url.fieldset, doctype: url.doctype}).dialog("open");
-}
+};
 
 // Button that opens a dialog for editing a fieldset
 
@@ -60,7 +60,7 @@ var editFieldsetButton = function(target) {
   });
   
   fieldsetDialog(url, oldobj).dialog("open");
-}
+};
 
 // Button that opens a dialog for deleting a fieldset
 
@@ -76,14 +76,14 @@ var deleteFieldsetButton = function(target) {
   if (confirm("Are you sure? This is permanent.")) {
     url.delete(complete, this);
   }
-}
+};
 
 // Button that opens a dialog for adding a fieldset
 
 var addFieldsetButton = function(target) {
   var url = cpath(target, "fieldset");
   fieldsetDialog(url, {doctype: url.doctype}).dialog("open");
-}
+};
 
 var editDoctypeButton = function(target) {
   var url = cpath(target, "doctype");
@@ -94,7 +94,7 @@ var editDoctypeButton = function(target) {
     oldobj[item] = getValue('doctype-' + item, target);
   });
   doctypeDialog(url, oldobj).dialog("open");
-}
+};
 
 var deleteDoctypeButton = function(target) {
   var url = cpath(target, "doctype");
@@ -107,22 +107,22 @@ var deleteDoctypeButton = function(target) {
   if (confirm("Are you sure? This is permanent.")) {
     url.delete(complete, this);
   }
-}
+};
 
 var addDoctypeButton = function(target) {
   var url = cpath(target, "doctype");
   doctypeDialog(url, {}).dialog("open");
-}
+};
 
 var addCharseqButton = function(target) {
   var url = cpath(target, "doctype");
   doctypeDialog(url, {}).dialog("open");
-}
+};
 
 // Action for click event on accordion head
 
 var accordionHead = function(target) {
   var url = cpath(target, "field");
   populateFields(url);
-}
+};
 
