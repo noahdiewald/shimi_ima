293a0e3273e7cf99e2beb7b3072db05d61c65da0
Noah Diewald <noah@diewald.me>
2011-10-10 22:44:27 -0500

    new records and types

--- include/types.hrl
+++ include/types.hrl
@@ -1,19 +1,21 @@
 % Types for use with CouchDB
 
--record(vq, {key :: jsn:json_term(), 
-             startkey :: jsn:json_term(), 
-             startkey_docid :: string(), 
-             endkey :: jsn:json_term(), 
-             endkey_docid :: string(),
-             limit :: jsn:json_number(), 
-             stale :: ok | update_after, 
-             descending = false :: boolean(), 
-             skip = 0 :: jsn:json_number(), 
-             group_level = exact :: jsn:json_number() | exact, 
-             reduce = true :: boolean(), 
-             include_docs = false :: boolean(), 
-             inclusive_end = true :: boolean(),
-             update_seq = false :: boolean()}).
+-record(vq, {
+  key :: jsn:json_term(), 
+  startkey :: jsn:json_term(), 
+  startkey_docid :: string(), 
+  endkey :: jsn:json_term(), 
+  endkey_docid :: string(),
+  limit :: jsn:json_number(), 
+  stale :: ok | update_after, 
+  descending = false :: boolean(), 
+  skip = 0 :: jsn:json_number(), 
+  group_level = exact :: jsn:json_number() | exact, 
+  reduce = true :: boolean(), 
+  include_docs = false :: boolean(), 
+  inclusive_end = true :: boolean(),
+  update_seq = false :: boolean()
+}).
 
 -type view_query() :: #vq{}.
 
@@ -38,3 +40,98 @@
 }).
 
 -type charseq() :: #charseq{}.
+
+-type subcategory() :: text | textarea | date | integer | rational | boolean | openboolean | select | multiselect | docselect | docmultiselect | file.
+
+-type basicval() :: number() | binary(). %TODO | date().
+
+-record(field, {
+  id :: binary(),
+  rev :: binary(),
+  allowed :: [basicval()],
+  category :: field,
+  charseq :: binary(),
+  default :: basicval(),
+  description :: binary(),
+  doctype :: binary(),
+  fieldset :: binary(),
+  head :: boolean(),
+  label :: boolean(),
+  max :: basicval(),
+  min :: basicval(),
+  name :: boolean(),
+  order :: integer(),
+  regex :: regex(),
+  required :: boolean(),
+  reversal :: boolean(),
+  source :: binary(),
+  subcategory :: subcategory()
+}).
+
+-record(fieldset, {
+  id :: binary(),
+  rev :: binary(),
+  category :: fieldset,
+  description :: binary(),
+  doctype :: binary(),
+  label :: boolean(),
+  name :: boolean(),
+  order :: integer(),
+  multiple :: boolean(),
+  collapse :: boolean()
+}).
+
+-record(doctype, {
+  id :: binary(),
+  rev :: binary(),
+  category :: doctype,
+  description :: binary()
+}).
+
+-type field() :: #field{}.
+-type fieldset() :: #fieldset{}.
+-type doctype() :: #doctype{}.
+
+-type anyval() :: basicval() | [binary()] | boolean() | null.
+
+-record(docfield, {
+  id :: binary(),
+  instance :: binary(),
+  charseq :: binary(),
+  head :: boolean(),
+  label :: boolean(),
+  max :: basicval(),
+  min :: basicval(),
+  name :: boolean(),
+  order :: integer(),
+  regex :: regex(),
+  required :: boolean(),
+  reversal :: boolean(),
+  subcategory :: subcategory(),
+  value :: anyval(),
+  sortkey :: binary()
+}).
+
+-type docfield() :: #docfield{}.
+
+-record(docfieldset, {
+  id :: binary(),
+  label :: boolean(),
+  name :: boolean(),
+  order :: integer(),
+  multiple :: boolean(),
+  collapse :: boolean(),
+  fields :: [docfield()] | [[docfield()]]
+}).
+
+-type docfieldset() :: #docfieldset{}.
+
+-record(document, {
+  id :: binary(),
+  rev :: binary(),
+  description :: binary(),
+  doctype :: binary(),
+  fieldsets :: [docfieldset()]
+}).
+
+-type document() :: #document{}.
