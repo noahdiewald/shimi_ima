965f13bd0b7900622a5cabc217e2a1cf9a7f1d09
Noah Diewald <noah@diewald.me>
2012-02-07 19:12:54 -0600

    fixed syntax

--- priv/www/js/documents/click-dispatch.js
+++ priv/www/js/documents/click-dispatch.js
@@ -2,20 +2,21 @@
 // event target. Takes an event object and performs an action.
 
 function clickDispatch(e) {
-  var action = dispatcher({
-    ".add-button span": function(t) {initFieldset(t.parent())},
-    ".remove-button span": function(t) {removeFieldset(t.parent())},
-    "#save-document-button span": function(t) {saveDoc(t.parent())},
-    "#create-document-button span": function(t) {createDoc(t.parent())},
-    "#clear-document-button span": function(t) {clearDoc(t.parent())},
-    "#document-edit-button span": function(t) {editDoc(t.parent())},
-    "#document-delete-button span": function(t) {deleteDoc(t.parent())},
-    "#document-restore-button span": function(t) {restoreDoc(t.parent())},
-    "#document-view-list > li > b": function(t) {collapseToggle(t)},
-    "#panel-toggle li": function(t) {panelToggle(t)},
-    ".expander": function(t) {toggleTextarea(t)},
-    "label span": function(t) {showHelpDialog(t)}
-  });
+  var action = dispatcher(
+    {
+      ".add-button span": function(t) {initFieldset(t.parent());},
+      ".remove-button span": function(t) {removeFieldset(t.parent());},
+      "#save-document-button span": function(t) {saveDoc(t.parent());},
+      "#create-document-button span": function(t) {createDoc(t.parent());},
+      "#clear-document-button span": function(t) {clearDoc(t.parent());},
+      "#document-edit-button span": function(t) {editDoc(t.parent());},
+      "#document-delete-button span": function(t) {deleteDoc(t.parent());},
+      "#document-restore-button span": function(t) {restoreDoc(t.parent());},
+      "#document-view-list > li > b": function(t) {collapseToggle(t);},
+      "#panel-toggle li": function(t) {panelToggle(t);},
+      ".expander": function(t) {toggleTextarea(t);},
+      "label span": function(t) {showHelpDialog(t);}
+    });
 
   action(e);
 }
