8039b9bd19692a8961c431ef94f9cd63706b5002
Noah Diewald <noah@diewald.me>
2012-09-22 21:35:06 -0500

    ability to turn off local caching #1102

--- priv/www/js/documents/fields-init.js
+++ priv/www/js/documents/fields-init.js
@@ -1,28 +1,3 @@
-function initFieldsets() {
-  $('fieldset').each(function(i, fieldset) {
-    initFieldset(fieldset);
-  });
-  
-  return true;
-}
-
-function initFieldset(fieldset, callback) {
-  var url = dpath($(fieldset), "fieldset");
-  var id = fsInfo("fieldset", $(fieldset));
-  var container = $('#container-' + id);
-  var appendIt = function(data) {
-    container.append(data);
-    initFields(container, callback);
-  };
-  var storeIt = function(data) {
-    localStorage.setItem(url, data);
-    appendIt(data);
-  };
-
-  ifStoredElse(url.toString(), appendIt, storeIt);
-
-  return false;
-}
 
 var ifStoredElse = function(key, success, otherwise) {
   var item = null;
@@ -36,7 +11,47 @@ var ifStoredElse = function(key, success, otherwise) {
   }
 };
 
-function initFields(container, callback) {
+var initFieldsets = function() {
+  var reload;
+  var container = $("#create-document-button");
+  var doctype = dInfo("doctype", container);
+  var versionKey = doctype + "_version";
+  var oldVersion = localStorage.getItem(versionKey);
+  var curVersion = dInfo("version", container);
+
+  reload = oldVersion != curVersion;
+  localStorage.setItem(versionKey, curVersion);
+
+  $('fieldset').each(function(i, fieldset) {
+    initFieldset(fieldset, false, reload);
+  });
+  
+  return true;
+}
+
+var initFieldset = function(fieldset, callback, reload) {
+  var url = dpath($(fieldset), "fieldset").toString();
+  var id = fsInfo("fieldset", $(fieldset));
+  var container = $('#container-' + id);
+  var appendIt = function(data) {
+    container.append(data);
+    initFields(container, callback, reload);
+  };
+  var storeIt = function(data) {
+    localStorage.setItem(url, data);
+    appendIt(data);
+  };
+
+  if (reload) {
+    localStorage.removeItem(url);
+  }
+
+  ifStoredElse(url.toString(), appendIt, storeIt);
+
+  return false;
+}
+
+var initFields = function(container, callback, reload) {
   var url = dpath(container, "field");
   var section = container.children('.fields').last();
   var prependIt = function(data) {
@@ -52,8 +67,11 @@ function initFields(container, callback) {
     prependIt(data);
   };
 
+  if (reload) {
+    localStorage.removeItem(url);
+  }
+
   ifStoredElse(url.toString(), prependIt, storeIt);
     
   return true;
 }
-
