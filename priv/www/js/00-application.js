d7ee03a8112a58541b0755831775774c25d8910e
Noah Diewald <noah@diewald.me>
2012-11-21 01:20:36 -0600

    moving code out of application file into new modules #1113

--- priv/www/js/00-application.js
+++ priv/www/js/00-application.js
@@ -6,71 +6,6 @@
  than one section and hasn't been moved to its own file can be found
  here.
  
- h2. Data Attribute Key Value Stores
- 
- There are two functions provided for getting values from keys
- embedded in HTML elements.
-  
-  @getValue(key, elem)@
-  
-  This funtion takes a key that corresponds to the name of the data
-  attribute without the data- prefix. It also takes a jQuery
-  object. The assumption is that the jQuery object will hold only one
-  element but it may work either way. The element is expected to have
-  an attribute data-group-id with a value that is the id of the
-  element actually holding the data.
- 
- Example:
-
- <pre> 
-   <div
-     id="someid"
-     data-fieldset-fieldset="fsid"
-     data-fieldset-doctype="did"></div>
-     
-   <div
-    id="thisid"
-    data-group-id="someid">
-    
-   getValue("fieldset-doctype", $(thisid)) == "did";
- </pre> 
-   
- The following also works:
-
- <pre> 
-   <div
-     id="someid2"
-     data-fieldset-fieldset="fsid"
-     data-fieldset-doctype="did"></div>
-   
-   <div
-     id="someid"
-     data-group-id="someid2"
-     data-fieldset-fieldset="fsid"></div>
-     
-   <div
-    id="thisid"
-    data-group-id="someid"></div>
-    
-   getValue("fieldset-doctype", $(thisid)) == "did";
- </pre> 
-   
-  @putValue(key, value, elem)@
-  
-  This function will set an attribute at the target with a name
-  corresponding to key and a value of value.
-
- == Click Dispatcher
- 
- Each section of the application calls this function with an object
- composed of keys of CSS patterns of elements which should have click
- event actions bound to them and values of functions that will be
- called if a click event occurs and the key pattern matches.
-
-  @dispatcher(patterns)@
-  
-  *More to come*
-  
  == Compatability Functions
  
  @Object.keys@ and @array.reduce@ compatibility functions from MDC
@@ -100,28 +35,6 @@
   fields.
   
 */
-
-function getValue(key, elem) {
-  var getValue1 = function(key, elem, id) {
-    var gid = elem.attr('data-group-id');
-    var store = $('#' + gid);
-    var val = store.attr('data-' + key);
-    var next = store.attr('data-group-id');
-    
-    if (val === undefined &&  next !== undefined &&  gid !== next) {
-      return getValue1.r(key, store, id);
-    }
-    
-    return id.r(val);
-  };
-  
-  return getValue1.t(key, elem, identity);
-}
-
-function putValue(key, value, elem) {
-  var dataElem = elem.attr('data-group-id');
-  $('#' + dataElem).attr('data-' + key, value);
-}
 
 // A predicate function to detect blankness
       
@@ -213,23 +126,6 @@ Array.prototype.trimAll = function() {
                               return !i.match(/^$/);
                             });
 };
-
-// Event dispatch
-
-function dispatcher(patterns) {
-  var d = function(e) {
-    var target = $(e.target);
-    
-    Object.keys(patterns).forEach(function(pattern) {
-      if (target.is(pattern)) {
-        var action = patterns[pattern];
-        action(target);
-      }
-    });  
-  };
-  
-  return d;
-}
 
 // Dialog form helpers
 
