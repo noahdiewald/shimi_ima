19a8748769bf477da95598c6c27f3812786c6d97
Noah Diewald <noah@diewald.me>
2013-02-13 03:02:21 -0600

    updating field_resource for cowboy #1295

--- src/field_resource.erl
+++ src/field_resource.erl
@@ -1,160 +1,176 @@
-%%% Copyright 2011 University of Wisconsin Madison Board of Regents.
+%%% Copyright 2012 University of Wisconsin Madison Board of Regents.
 %%%
-%%% This file is part of dictionary_maker.
+%%% This file is part of Ʃimi Ima.
 %%%
-%%% dictionary_maker is free software: you can redistribute it and/or modify
-%%% it under the terms of the GNU General Public License as published by
-%%% the Free Software Foundation, either version 3 of the License, or
-%%% (at your option) any later version.
+%%% dictionary_maker is free software: you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation, either version 3 of the
+%%% License, or (at your option) any later version.
 %%%
 %%% dictionary_maker is distributed in the hope that it will be useful,
 %%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-%%% GNU General Public License for more details.
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%%% General Public License for more details.
 %%%
-%%% You should have received a copy of the GNU General Public License
-%%% along with dictionary_maker. If not, see <http://www.gnu.org/licenses/>.
+%%% You should have received a copy of the GNU General
+%%% Public License along with dictionary_maker. If not, see
+%%% <http://www.gnu.org/licenses/>.
 
-%%% @copyright 2011 University of Wisconsin Madison Board of Regents.
+%%% @copyright 2012 University of Wisconsin Madison Board of Regents.
 %%% @version {@version}
 %%% @author Noah Diewald <noah@diewald.me>
 %%% @doc Dictionary Maker resource for dealing with fields.
 
 -module(field_resource).
+-author('Noah Diewald <noah@diewald.me>').
 
-% Webmachine API
+-export([init/3]).
 -export([
          allowed_methods/2,
          content_types_provided/2,
-         init/1, 
          is_authorized/2,
          resource_exists/2,
+         rest_init/2,
          to_html/2,
          to_json/2
         ]).
-
-% Custom
 -export([
          validate_authentication/3
         ]).
 
--include_lib("webmachine/include/webmachine.hrl").
--include_lib("include/types.hrl").
+-include_lib("types.hrl").
 
-% Standard webmachine functions
+init(_Transport, _R, _S) -> {upgrade, protocol, cowboy_rest}.
 
-init(Opts) -> {ok, Opts}.
+rest_init(R, S) -> {ok, R, S}.
 
 resource_exists(R, S) ->
     case proplists:get_value(target, S) of
-        identifier -> identifier_exists(h:id(R), R, S);
-        index -> index_exists(h:fieldset(R), R, S)
+        identifier -> 
+            {Id, R1} = h:id(R),
+            identifier_exists(Id, R1, S);
+        index ->
+            {Fieldset, R1} = h:fieldset(R),
+            index_exists(Fieldset, R1, S)
     end. 
 
 identifier_exists(Id, R, S) ->
     case field:is_meta(Id) of
         true -> {true, R, [{is_meta, list_to_binary(Id)}|S]};
-        _ -> {h:exists(Id, R, S), R, S}
+        _ -> 
+            {Exist, R1} = h:exists(Id, R, S),
+            {Exist, R1, S}
     end.
 
 index_exists("metadata", R, S) -> {true, R, S};
-index_exists(Fieldset, R, S) -> {h:exists(Fieldset, R, S), R, S}.
+index_exists(Fieldset, R, S) -> 
+    {Exist, R1} = h:exists(Fieldset, R, S),
+    {Exist, R1, S}.
 
 is_authorized(R, S) ->
     proxy_auth:is_authorized(R, [{source_mod, ?MODULE}|S]).
 
 allowed_methods(R, S) ->
-    {['HEAD', 'GET'], R, S}.
+    {[<<"HEAD">>, <<"GET">>], R, S}.
 
 content_types_provided(R, S) ->
     % Currently having a problem with jquery and Accepts headers
-    % this is a work around.
-    case wrq:get_qs_value("format", R) of
-        undefined -> {[{"text/html", to_html}, 
-                       {"application/json", to_json}], R, S};
-        "json" -> {[{"*/*", to_json}], R, S};
-        "html" -> {[{"*/*", to_html}], R, S}
+    % this is a work around. TODO: verify newer version has problem.
+    {Format, R1} = cowboy_req:qs_value(<<"format">>, R),
+    case Format of
+        undefined -> {[{{<<"text">>, <<"html">>, []}, to_html}, 
+                       {{<<"application">>, <<"json">>, []}, to_json}], R1, S};
+        <<"json">> -> {[{{<<"*">>, <<"*">>, []}, to_json}], R1, S};
+        <<"html">> -> {[{{<<"*">>, <<"*">>, []}, to_html}], R1, S}
     end.
 
 to_json(R, S) ->
     case proplists:get_value(target, S) of
-        index -> {json_fields(R, S), R, S};
-        identifier -> {json_field(R, S), R, S}
+        index ->
+            {Json, R1} = json_fields(R, S),
+            {Json, R1, S};
+        identifier -> 
+            {Json, R1} = json_field(R, S),
+            {Json, R1, S}
     end.
   
 to_html(R, S) ->
     case proplists:get_value(target, S) of
-        index -> {html_fields(R, S), R, S};
-        identifier -> {html_field(R, S), R, S}
+        index ->
+            {Html, R1} = html_fields(R, S),
+            {Html, R1, S};
+        identifier ->
+            {Html, R1} = html_field(R, S),
+            {Html, R1, S}
     end.
   
 % Helpers
 
 json_field(R, S) ->
-    Field = case proplists:get_value(is_meta, S) of
-                undefined ->
-                    {ok, Json} = h:id_data(R, S),
-                    Subcategory = 
-                        binary_to_list(jsn:get_value(<<"subcategory">>, Json)),
-                    get_allowed(Subcategory, Json, h:project(R), S);
-                Id -> field:meta_field(Id)
-            end,
-    jsn:encode(Field).
+    case proplists:get_value(is_meta, S) of
+        undefined ->
+            {{ok, Json}, R1} = h:id_data(R, S),
+            {Project, R2} = h:project(R1),
+            Subcategory = binary_to_list(jsn:get_value(<<"subcategory">>, Json)),
+            Field = get_allowed(Subcategory, Json, Project, S),
+            {jsn:encode(Field), R2};
+        Id -> 
+            Field = field:meta_field(Id),
+            {jsn:encode(Field), R}
+    end.
 
 json_fields(R, S) -> 
-    Doctype = h:doctype(R),
-    Fieldset = h:fieldset(R),
-    Project = h:project(R),
+    {[Doctype, Fieldset, Project], R1} = h:g([doctype, fieldset, project], R),
     {ok, Json} = q:field(Doctype, Fieldset, Project, S),
     Rows = jsn:get_value(<<"rows">>, Json),
   
     F = fun(Row) ->
                 Doc = jsn:get_value(<<"doc">>, Row),
-                Subcategory = binary_to_list(
-                                jsn:get_value(<<"subcategory">>, Doc)),
+                Subcategory = binary_to_list(jsn:get_value(<<"subcategory">>, Doc)),
                 get_allowed(Subcategory, Doc, Project, S)
         end,
   
-    jsn:encode(lists:map(F, Rows)).
+    {jsn:encode(lists:map(F, Rows)), R1}.
 
 html_field(R, S) -> 
-     {ok, Json} = h:id_data(R, S),
-     get_field_html(Json, R, S).
+     {{ok, Json}, R1} = h:id_data(R, S),
+     get_field_html(Json, R1, S).
   
 html_fields(R, S) -> 
-    case wrq:get_qs_value("as", R) of
-        undefined -> html_as_fieldset(R, S);
-        "fieldset" -> html_as_fieldset(R, S);
-        "options" -> html_as_options(R, S)
+    {As, R1} = cowboy_req:qs_value(<<"as">>, R),
+    case As of
+        undefined -> html_as_fieldset(R1, S);
+        <<"fieldset">> -> html_as_fieldset(R1, S);
+        <<"options">> -> html_as_options(R1, S)
     end.
 
 html_as_fieldset(R, S) -> 
-    Doctype = h:doctype(R),
-    Fieldset = h:fieldset(R),
-    Project = h:project(R),
+    {[Doctype, Fieldset, Project], R1} = h:g([doctype, fieldset, project], R),
     {ok, Json} = q:field(Doctype, Fieldset, Project, S),
     Rows = jsn:get_value(<<"rows">>, Json),
   
-    F = fun(Row) ->
-                get_field_html(jsn:get_value(<<"doc">>, Row), R, S)
+    F = fun(Row, {RX, Acc}) ->
+                {Field, RY} = get_field_html(jsn:get_value(<<"doc">>, Row), RX, S),
+                {RY, [Field|Acc]}
         end,
   
-    lists:map(F, Rows).
+    {R2, Fieldsets} = lists:foldl(F, {R1, []}, Rows),
+    {lists:reverse(Fieldsets), R2}.
   
 html_as_options(R, S) ->
-    Json = option_list(R, S),
+    {Json, R1} = option_list(R, S),
     {ok, Html} = render:render(options_dtl, Json),
-    Html.
+    {Html, R1}.
 
 get_field_html(Json, R, S) ->
+    {Project, R1} = h:project(R),
     % One time use identifier
     UUID = utils:uuid(),
     Json1 = jsn:set_value(<<"instance_id">>, list_to_binary(UUID), Json),
-  
     Subcategory = binary_to_list(jsn:get_value(<<"subcategory">>, Json1)),
     Template = list_to_atom("field_" ++ Subcategory ++ "_dtl"),
-    {ok, Html} = Template:render(get_allowed(Subcategory, Json1, h:project(R), S)),
-    Html.
+    {ok, Html} = Template:render(get_allowed(Subcategory, Json1, Project, S)),
+    {Html, R1}.
 
 get_allowed([$d, $o, $c|_], Json, Project, S) -> get_allowed_docs(Json, Project, S);
 get_allowed("file", Json, Project, S) -> get_allowed_files(Json, Project, S);
@@ -176,13 +192,13 @@ get_allowed_files(Json, Project, S) ->
     jsn:set_value(<<"allowed">>, jsn:get_value(<<"rows">>, RawAllowed), Json).
       
 validate_authentication(Props, R, S) ->
-    {ok, ProjectData} = h:project_data(R, S),
+    {{ok, ProjectData}, R1} = h:project_data(R, S),
     Name = jsn:get_value(<<"name">>, ProjectData),
     ValidRoles = [<<"_admin">>, <<"manager">>, Name],
     IsMember = fun (Role) -> lists:member(Role, ValidRoles) end,
     case lists:any(IsMember, proplists:get_value(<<"roles">>, Props)) of
         true -> {true, R, S};
-        false -> {proplists:get_value(auth_head, S), R, S}
+        false -> {proplists:get_value(auth_head, S), R1, S}
     end.
 
 option_list(R, S) ->
@@ -191,11 +207,10 @@ option_list(R, S) ->
                 Id = jsn:get_value(<<"id">>, X),
                 [{<<"key">>, Label}, {<<"value">>, Name}, {<<"id">>, Id}]
         end,
-    case h:fieldset(R) of
-        "metadata" -> field:meta_options();
+    {[Doctype, Fieldset, Project], R1} = h:g([doctype, fieldset, project], R),
+    case Fieldset of
+        <<"metadata">> -> {field:meta_options(), R1};
         Fieldset -> 
-            {ok, Json} = q:field(h:doctype(R), Fieldset, false, h:project(R), S),
-            jsn:set_value(<<"rows">>, 
-                          lists:map(F, jsn:get_value(<<"rows">>, Json)), 
-                          Json)
+            {ok, Json} = q:field(Doctype, Fieldset, false, Project, S),
+            {jsn:set_value(<<"rows">>, lists:map(F, jsn:get_value(<<"rows">>, Json)), Json), R1}
     end.
