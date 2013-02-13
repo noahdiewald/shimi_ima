0b4ba3b654a3063aff5e5aadfc1039ff04b7fa79
Noah Diewald <noah@diewald.me>
2013-02-12 23:05:58 -0600

    updating src/config_field_resource for cowboy #1295

--- src/config_field_resource.erl
+++ src/config_field_resource.erl
@@ -1,75 +1,85 @@
-%% @author Noah Diewald <noah@diewald.me>
-%% @copyright 2010 University of Wisconsin Madison Board of Regents.
-%% Copyright (c) 2010 University of Wisconsin Madison Board of Regents
-%%
-%% Permission is hereby granted, free of charge, to any person obtaining
-%% a copy of this software and associated documentation files (the
-%% "Software"), to deal in the Software without restriction, including
-%% without limitation the rights to use, copy, modify, merge, publish,
-%% distribute, sublicense, and/or sell copies of the Software, and to
-%% permit persons to whom the Software is furnished to do so, subject to
-%% the following conditions:
-%%
-%% The above copyright notice and this permission notice shall be included
-%% in all copies or substantial portions of the Software.
-%%
-%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-%% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-%% NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-%% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-%% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
-%% THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-%% @doc The resource used accessing and editing fields in a configuration
-%%      context.
+%%% Copyright 2012 University of Wisconsin Madison Board of Regents.
+%%%
+%%% This file is part of Ʃimi Ima.
+%%%
+%%% dictionary_maker is free software: you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation, either version 3 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% dictionary_maker is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General
+%%% Public License along with dictionary_maker. If not, see
+%%% <http://www.gnu.org/licenses/>.
+
+%%% @copyright 2012 University of Wisconsin Madison Board of Regents.
+%%% @version {@version}
+%%% @author Noah Diewald <noah@diewald.me>
+%%% @doc The resource used accessing and editing fields in a configuration
+%%%     context.
 
 -module(config_field_resource).
+-author('Noah Diewald <noah@diewald.me>').
 
-% Webmachine API
+-export([init/3]).
 -export([
-  allowed_methods/2,
-  content_types_accepted/2,
-  content_types_provided/2,
-  create_path/2,
-  delete_resource/2,
-  from_json/2,
-  init/1, 
-  is_authorized/2,
-  post_is_create/2,
-  resource_exists/2,
-  id_html/2,
-  index_html/2
-]).
-
-% Custom
+         allowed_methods/2,
+         content_types_accepted/2,
+         content_types_provided/2,
+         create_path/2,
+         delete_resource/2,
+         from_json/2,
+         id_html/2,
+         index_html/2,
+         is_authorized/2,
+         post_is_create/2,
+         resource_exists/2,
+         rest_init/2
+        ]).
 -export([
-  validate_authentication/3
-]).
+         validate_authentication/3
+        ]).
 
--include_lib("webmachine/include/webmachine.hrl").
+init(_Transport, _R, _S) -> {upgrade, protocol, cowboy_rest}.
 
-% Standard webmachine functions
-
-init(Opts) -> {ok, Opts}.
+rest_init(R, S) -> {ok, R, S}.
 
 resource_exists(R, S) ->
   case proplists:get_value(target, S) of
     index -> 
-      case h:exists(h:doctype(R), R, S) of
-        true -> {h:exists(h:fieldset(R), R, S), R, S};
-        F -> {F, R, S}
+      {Doctype, R1} = h:doctype(R),
+      {Exist1, R2} = h:exists(Doctype, R1, S),
+      case Exist1 of
+        true -> 
+          {Fieldset, R3} = h:fieldset(R2),
+          {Exist2, R4} = h:exists(Fieldset, R3, S),
+          {Exist2, R4, S};
+        F -> {F, R2, S}
       end;
-    identifier -> {h:exists(h:id(R), R, S), R, S}
+    identifier -> h:exists_id(R, S)
   end. 
 
 is_authorized(R, S) ->
-  proxy_auth:is_authorized(R, [{source_mod, ?MODULE}|S]).
+    proxy_auth:is_authorized(R, [{source_mod, ?MODULE}|S]).
 
 allowed_methods(R, S) ->
-  case proplists:get_value(target, S) of
-    index -> {['HEAD', 'GET', 'POST'], R, S};
-    identifier -> {['HEAD', 'GET', 'PUT', 'DELETE'], R, S}
-  end.
+    case proplists:get_value(target, S) of
+        index -> {[<<"HEAD">>, <<"GET">>, <<"POST">>], R, S};
+        identifier -> {[<<"HEAD">>, <<"GET">>, <<"PUT">>, <<"DELETE">>], R, S}
+    end.
+
+content_types_accepted(R, S) ->
+    h:accept_json(R, S).
+
+content_types_provided(R, S) ->
+    case proplists:get_value(target, S) of
+        index -> {[{{<<"text">>, <<"html">>, []}, index_html}], R, S};
+        identifier -> {[{{<<"text">>, <<"html">>, []}, id_html}], R, S}
+    end.
   
 delete_resource(R, S) ->
     h:delete(R, S).
@@ -78,38 +88,23 @@ post_is_create(R, S) ->
   {true, R, S}.
 
 create_path(R, S) ->
-  Json = jsn:decode(wrq:req_body(R)),
-  
-  {Id, Json1} = case jsn:get_value(<<"_id">>, Json) of
-    undefined -> 
-      GenId = utils:uuid(),
-      {GenId, jsn:set_value(<<"_id">>, list_to_binary(GenId), Json)};
-    IdBin -> {binary_to_list(IdBin), Json}
-  end,
-  
-  Location = "http://" ++ wrq:get_req_header("host", R) ++ "/" ++ wrq:path(R) ++ "/" ++ Id,
-  R1 = wrq:set_resp_header("Location", Location, R),
-  
-  {Id, R1, [{posted_json, Json1}|S]}.
-
-content_types_provided(R, S) ->
-  case proplists:get_value(target, S) of
-    index -> {[{"text/html", index_html}], R, S};
-    identifier -> {[{"text/html", id_html}], R, S}
-  end.
-  
-content_types_accepted(R, S) ->
-  {[{"application/json", from_json}], R, S}.
+    {Body, R1} = cowboy_req:body(R),
+    Json = jsn:decode(Body),
+    {Id, Json1} = case jsn:get_value(<<"_id">>, Json) of
+                      undefined -> 
+                          GenId = list_to_binary(utils:uuid()),
+                          {GenId, jsn:set_value(<<"_id">>, GenId, Json)};
+                      IdBin -> {IdBin, Json}
+                  end,
+    {Id, R1, [{posted_json, Json1}|S]}.
   
 index_html(R, S) ->
-    Doctype = h:doctype(R),
-    Fieldset = h:fieldset(R),
-    Project = h:project(R),
+    {[Doctype, Fieldset, Project], R1} = g:h([doctype, fieldset, project], R),
     {ok, Json} = q:field(Doctype, Fieldset, Project, S),
     Rows = jsn:get_value(<<"rows">>, Json),
     Fields = field:arrange(Rows),
-    {render:renderings([{<<"rows">>, Fields}], 
-                       config_field_list_elements_dtl), R, S}.
+    Html = render:renderings([{<<"rows">>, Fields}], config_field_list_elements_dtl),
+    {Html, R1, S}.
 
 id_html(R, S) ->
     h:id_html(config_fieldset_dtl,  R, S).
@@ -121,21 +116,22 @@ from_json(R, S) ->
   end.
 
 json_create(R, S) ->  
-    {ok, updated} = h:update_doctype_version(R, S),
+    {{ok, updated}, R1} = h:update_doctype_version(R, S),
     Json = proplists:get_value(posted_json, S),
-    h:create(Json, R, S).
+    h:create(Json, R1, S).
   
 json_update(R, S) ->
-    {ok, updated} = h:update_doctype_version(R, S),
-    Json = jsn:decode(wrq:req_body(R)),
-    h:update(Json, R, S).
+    {{ok, updated}, R1} = h:update_doctype_version(R, S),
+    {Body, R2} = cowboy_req:body(R1),
+    Json = jsn:decode(Body),
+    h:update(Json, R2, S).
       
 % Helpers
 
 validate_authentication(Props, R, S) ->
-  ValidRoles = [<<"_admin">>, <<"manager">>],
-  IsMember = fun (Role) -> lists:member(Role, ValidRoles) end,
-  case lists:any(IsMember, proplists:get_value(<<"roles">>, Props)) of
-    true -> {true, R, S};
-    false -> {proplists:get_value(auth_head, S), R, S}
-  end.
+    ValidRoles = [<<"_admin">>, <<"manager">>],
+    IsMember = fun (Role) -> lists:member(Role, ValidRoles) end,
+    case lists:any(IsMember, proplists:get_value(<<"roles">>, Props)) of
+        true -> {true, R, S};
+        false -> {proplists:get_value(auth_head, S), R, S}
+    end.
