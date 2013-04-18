cdd8fa44362f04e2ce567e47f18a90cf984269dc
Noah Diewald <noah@diewald.me>
2013-04-18 02:44:34 -0500

    modularizing update handler in anticipation of #1386

---
+++ priv/src/server/shimi_ima/lib/update_helpers.js
@@ -0,0 +1,41 @@
+exports.stamp = function (newDoc, doc, req) {
+  var now = (new Date()).toUTCString();
+  var message;
+
+  if (!doc) {
+    if (newDoc._id) {
+      newDoc.created_at_ = now;
+      newDoc.created_by_ = req.userCtx.name;
+      message = {
+        id: newDoc._id,
+        timestamp: newDoc.created_at_,
+        user: newDoc.created_by_
+      };
+    } else {
+      newDoc = null;
+      message = 'This application expects the document _id in the JSON body';
+    }
+  } else {
+
+    newDoc.updated_at_ = now;
+    newDoc.updated_by_ = req.userCtx.name;
+    newDoc.created_at_ = doc.created_at_;
+
+    if (doc.create_user_) {
+      newDoc.created_by_ = doc.create_user_;
+    } else {
+      newDoc.created_by_ = doc.created_by_;
+    }
+
+    message = {
+      id: newDoc._id,
+      timestamp: newDoc.updated_at_,
+      user: newDoc.updated_by_,
+      changes: newDoc.changes
+    };
+
+    newDoc.prev_ = doc._rev;
+  }
+
+  return {doc: newDoc, message: message};
+};
