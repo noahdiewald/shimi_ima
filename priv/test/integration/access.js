34cd3dba865e31bff13fd785b2fb3e0f6e7fd25b
Noah Diewald <noah@diewald.me>
2013-05-26 04:21:12 -0500

    access tests

--- priv/test/integration/access.js
+++ priv/test/integration/access.js
@@ -1,10 +1,8 @@
 var casper = require('casper').create();
 
 casper.start('http://127.0.0.1:8000/projects', function() {
-    casper.test.assertHttpStatus(200, 'login accepted');
+    casper.test.assertHttpStatus(401, 'no valid login');
 });
-
-casper.setHttpAuth('tester', 'tester');
 
 casper.run(function() {
     this.test.renderResults(true);
