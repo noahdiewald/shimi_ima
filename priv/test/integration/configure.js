7c41d7ec16e01eab3fb6237e14192835f8daf638
Noah Diewald <noah@diewald.me>
2013-06-01 02:15:14 -0500

    basic field creation

--- priv/test/integration/configure.js
+++ priv/test/integration/configure.js
@@ -3,6 +3,7 @@ exports.configure = function (project, casper)
 {
   'use strict';
   var projectUrlPart = 'project-' + project;
+  var firstFieldsetId;
 
   // Follow configure link from projects page
   casper.then(function ()
@@ -39,7 +40,6 @@ exports.configure = function (project, casper)
   // Add fieldset
   casper.then(function ()
   {
-    var fieldsetId;
     var title = 'Add fieldset: ';
 
     casper.echo(title);
@@ -56,12 +56,45 @@ exports.configure = function (project, casper)
     {
       casper.test.assertTextExists('First Fieldset', title + 'fieldset label is found on page');
       casper.test.assertExists('#fieldsets-FirstDoctype h3', title + 'fieldset h3 exists');
-      fieldsetId = casper.evaluate(function ()
+      firstFieldsetId = casper.evaluate(function ()
       {
         return document.querySelectorAll('#fieldsets-FirstDoctype h3')[0].id.substring(5);
       });
-      casper.click('#head-' + fieldsetId + ' a');
-      casper.test.assertExists('#' + fieldsetId, title + 'fieldset exists');
+      casper.click('#head-' + firstFieldsetId + ' a');
+      casper.waitUntilVisible('#' + firstFieldsetId, function ()
+      {
+        casper.test.assertExists('#' + firstFieldsetId, title + 'fieldset exists');
+      });
+    });
+  });
+
+  // Add field
+  casper.then(function ()
+  {
+    var fieldId;
+    var title = 'Add field: ';
+
+    casper.echo(title);
+    casper.click('#add-field-to-' + firstFieldsetId);
+    casper.waitWhileVisible('#loading', function ()
+    {
+      casper.fill('#field-dialog form',
+      {
+        'field-name-input': 'first_field',
+        'field-label-input': 'First Field',
+        'field-subcategory-input': 'text',
+        'field-head-input': true
+      }, false);
+      casper.clickLabel('Save', 'span');
+    });
+    casper.waitWhileVisible('#loading', function ()
+    {
+      casper.test.assertExists('.field-row', title + 'a new field row exists');
+      fieldId = casper.evaluate(function ()
+      {
+        return document.querySelectorAll('.field-row')[0].id;
+      });
+      casper.test.assertExists('#' + fieldId, title + 'field exists');
     });
   });
 
