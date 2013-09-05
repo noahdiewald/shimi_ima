dc58bc653a00562f7cdc1bdd68ecceff69045d43
Noah Diewald <noah@diewald.me>
2013-09-05 05:14:25 -0500

    viewui.js commonjs conversion #1889

--- priv/src/client/documents/viewui.js
+++ priv/src/client/documents/viewui.js
@@ -1,386 +1,425 @@
-// View pane UI elements
-shimi.viewui = (function (args)
+// # The view user interface
+//
+// *Implicit depends:* DOM, JQuery, Hogan, templates
+//
+// View pane UI elements.
+//
+// *TODO* I may be exporting more than needed.
+
+// Variable Definitions
+
+var store = require('../store.js').store;
+var indexui = require('./indexui.js');
+var flash = require('../flash.js');
+var editui = require('./editui.js');
+var fieldsets = require('./fieldsets.js');
+
+// Internal functions
+
+// User interface element
+var dv = function ()
 {
-  'use strict';
+  return $('#document-view');
+};
 
-  var mod = {};
-  var dv = function ()
-  {
-    return $('#document-view');
-  };
-  var dvt = function ()
-  {
-    return $('#document-view-tree');
-  };
-  var viewInfo = function ()
-  {
-    return $('#document-view-info');
-  };
+// User interface element
+var dvt = function ()
+{
+  return $('#document-view-tree');
+};
 
-  // Make an object where fieldsets with deletions are identified.
-  var getDeletions = function (changes)
+// User interface element
+var viewInfo = function ()
+{
+  return $('#document-view-info');
+};
+
+// Make an object where fieldsets with deletions are identified.
+var getDeletions = function (changes)
+{
+  return Object.keys(changes).reduce(function (acc, x)
   {
-    return Object.keys(changes).reduce(function (acc, x)
+    // If it was changed and there is no new value, it was deleted.
+    if (changes[x].newValue === undefined)
     {
-      // If it was changed and there is no new value, it was deleted.
-      if (changes[x].newValue === undefined)
+      if (acc[changes[x].fieldset] === undefined)
       {
-        if (acc[changes[x].fieldset] === undefined)
-        {
-          acc[changes[x].fieldset] = {};
-        }
-        acc[changes[x].fieldset][x] = changes[x];
+        acc[changes[x].fieldset] = {};
       }
-
-      return acc;
-    },
-    {});
-  };
-
-  var processIncoming = function (docJson, rev)
-  {
-    var withDeletions = {};
-
-    if (docJson.changes)
-    {
-      withDeletions = getDeletions(docJson.changes);
+      acc[changes[x].fieldset][x] = changes[x];
     }
 
-    docJson.fieldsets.forEach(function (fset)
+    return acc;
+  },
+  {});
+};
+
+// Process the document from the server.
+var processIncoming = function (docJson, rev)
+{
+  var withDeletions = {};
+
+  if (docJson.changes)
+  {
+    withDeletions = getDeletions(docJson.changes);
+  }
+
+  docJson.fieldsets.forEach(function (fset)
+  {
+    var fsetId = fset.id;
+
+    if (withDeletions[fsetId] !== undefined)
     {
-      var fsetId = fset.id;
+      fset.removal = true;
+      fset.altered = true;
+    }
 
-      if (withDeletions[fsetId] !== undefined)
+    var fieldFunc = function (field)
+    {
+      var changes = {};
+      var change;
+
+      if (docJson.changes)
       {
-        fset.removal = true;
+        changes = docJson.changes;
+      }
+      change = changes[field.instance];
+
+      field.json_value = JSON.stringify(field.value);
+
+      if (change !== undefined)
+      {
+        field.changed = true;
         fset.altered = true;
+
+        if (change.originalValue === undefined)
+        {
+          fset.addition = true;
+          field.newfield = true;
+        }
+        else
+        {
+          field.originalValue = JSON.parse(change.originalValue);
+        }
       }
 
-      var fieldFunc = function (field)
+      if (field.subcategory === 'textarea')
       {
-        var changes = {};
-        var change;
-
-        if (docJson.changes)
-        {
-          changes = docJson.changes;
-        }
-        change = changes[field.instance];
-
-        field.json_value = JSON.stringify(field.value);
-
-        if (change !== undefined)
-        {
-          field.changed = true;
-          fset.altered = true;
-
-          if (change.originalValue === undefined)
-          {
-            fset.addition = true;
-            field.newfield = true;
-          }
-          else
-          {
-            field.originalValue = JSON.parse(change.originalValue);
-          }
-        }
-
-        if (field.subcategory === 'textarea')
-        {
-          field.is_textarea = true;
-        }
-        else if (field.value && field.subcategory.match('multi'))
-        {
-          field.value = field.value.join(', ');
-        }
-
-        return true;
-      };
-
-      if (fset.multiple)
-      {
-        fset.multifields.forEach(function (mfs)
-        {
-          mfs.fields.forEach(function (field)
-          {
-            fieldFunc(field);
-            return true;
-          });
-        });
+        field.is_textarea = true;
       }
-      else
+      else if (field.value && field.subcategory.match('multi'))
       {
-        fset.fields.forEach(function (field)
+        field.value = field.value.join(', ');
+      }
+
+      return true;
+    };
+
+    if (fset.multiple)
+    {
+      fset.multifields.forEach(function (mfs)
+      {
+        mfs.fields.forEach(function (field)
         {
           fieldFunc(field);
           return true;
         });
-      }
-
-      return true;
-    });
+      });
+    }
+    else
+    {
+      fset.fields.forEach(function (field)
+      {
+        fieldFunc(field);
+        return true;
+      });
+    }
 
     return true;
-  };
+  });
 
-  mod.formatTimestamps = function ()
+  return true;
+};
+
+// Exported functions
+
+// Format the 'update at' and 'created at' timestamps and localize them
+// to the current time zone.
+var formatTimestamps = function ()
+{
+  $('.timestamp').each(
+
+  function (i, item)
   {
-    $('.timestamp').each(
-
-    function (i, item)
+    var newDate = (new Date($(item).text())).toLocaleString();
+    if (newDate !== 'Invalid Date')
     {
-      var newDate = (new Date($(item).text())).toLocaleString();
-      if (newDate !== 'Invalid Date')
-      {
-        $(item).text(newDate);
-      }
-    });
+      $(item).text(newDate);
+    }
+  });
 
-    return mod;
-  };
+  return true;
+};
 
-  mod.get = function (id, rev, callback)
+// Get the document.
+var get = function (id, rev, callback)
+{
+  var url = 'documents/' + id;
+  var htmlTarget = dv();
+  var tmpl;
+
+  if (rev)
   {
-    var url = 'documents/' + id;
-    var htmlTarget = dv();
-    var tmpl;
+    url = url + '/' + rev;
+    htmlTarget = dvt();
+    tmpl = function (docJson)
+    {
+      return templates['document-view-tree'].render(docJson,
+      {
+        'document-view-field': templates['document-view-field']
+      });
+    };
+  }
+  else
+  {
+    tmpl = function (docJson)
+    {
+      return templates['document-view'].render(docJson,
+      {
+        'document-view-tree': templates['document-view-tree'],
+        'document-view-field': templates['document-view-field']
+      });
+    };
+
+  }
+
+  $.getJSON(url, function (docJson)
+  {
+    var documentHtml;
+
+    processIncoming(docJson, rev);
+    documentHtml = tmpl(docJson);
+    htmlTarget.html(documentHtml);
+    window.location.hash = id;
+    formatTimestamps();
+    dv().fadeTo('slow', 1);
+    if (callback)
+    {
+      callback();
+    }
 
     if (rev)
     {
-      url = url + '/' + rev;
-      htmlTarget = dvt();
-      tmpl = function (docJson)
-      {
-        return templates['document-view-tree'].render(docJson,
-        {
-          'document-view-field': templates['document-view-field']
-        });
-      };
+      $('#document-view-tree').addClass('oldrev');
     }
     else
     {
-      tmpl = function (docJson)
+      var restoreButton = $('#document-restore-button');
+      var editButton = $('#document-edit-button');
+      var deleteButton = $('#document-delete-button');
+
+      if (store(restoreButton).d('deleted') === 'true')
       {
-        return templates['document-view'].render(docJson,
+        editButton.hide();
+        deleteButton.hide();
+        restoreButton.show();
+      }
+    }
+  });
+
+  return true;
+};
+
+// Restore the state of a document to that of an earlier revision.
+var restore = function (id, rev)
+{
+  var url = './documents/' + id + '?rev=' + rev;
+  var restoreButton = $('#document-restore-button');
+  var skey = $('#first-index-element').attr('data-first-key');
+  var sid = $('#first-index-element').attr('data-first-id');
+  var body;
+  var title;
+
+  $.ajax(
+  {
+    type: 'DELETE',
+    url: url,
+    dataType: 'json',
+    contentType: 'application/json',
+    complete: function (req, status)
+    {
+      if (req.status === 200)
+      {
+        title = 'Success';
+        body = 'Your document was restored.';
+
+        get(id, null, function ()
         {
-          'document-view-tree': templates['document-view-tree'],
-          'document-view-field': templates['document-view-field']
+          dv().fadeTo('slow', 1);
+          indexui.get(skey, sid);
         });
-      };
+        flash.highlight(title, body);
+      }
+      else if (req.status === 409)
+      {
+        body = JSON.parse(req.responseText);
+        title = req.statusText;
 
+        flash.error(title, body.message);
+      }
+      else if (req.status === 404)
+      {
+        body = 'Document was erased and cannot be restored.';
+        title = req.statusText;
+
+        flash.error(title, body);
+      }
     }
+  });
 
-    $.getJSON(url, function (docJson)
-    {
-      var documentHtml;
+  return true;
+};
 
-      processIncoming(docJson, rev);
-      documentHtml = tmpl(docJson);
-      htmlTarget.html(documentHtml);
-      window.location.hash = id;
-      mod.formatTimestamps();
-      dv().fadeTo('slow', 1);
-      if (callback)
-      {
-        callback();
-      }
+// Delete the document.
+var del = function (id, rev)
+{
+  var url = './documents/' + id + '?rev=' + rev;
+  var restoreButton = $('#document-restore-button');
+  var skey = $('#first-index-element').attr('data-first-key');
+  var sid = $('#first-index-element').attr('data-first-id');
+  var body;
+  var title;
 
-      if (rev)
-      {
-        $('#document-view-tree').addClass('oldrev');
-      }
-      else
-      {
-        var restoreButton = $('#document-restore-button');
-        var editButton = $('#document-edit-button');
-        var deleteButton = $('#document-delete-button');
-
-        if (shimi.store(restoreButton).d('deleted') === 'true')
-        {
-          editButton.hide();
-          deleteButton.hide();
-          restoreButton.show();
-        }
-      }
-    });
-
-    return mod;
-  };
-
-  mod.restore = function (id, rev)
+  $.ajax(
   {
-    var url = './documents/' + id + '?rev=' + rev;
-    var restoreButton = $('#document-restore-button');
-    var skey = $('#first-index-element').attr('data-first-key');
-    var sid = $('#first-index-element').attr('data-first-id');
-    var body;
-    var title;
-
-    $.ajax(
+    type: 'DELETE',
+    url: url,
+    dataType: 'json',
+    contentType: 'application/json',
+    complete: function (req, status)
     {
-      type: 'DELETE',
-      url: url,
-      dataType: 'json',
-      contentType: 'application/json',
-      complete: function (req, status)
+      if (req.status === 200)
       {
-        if (req.status === 200)
-        {
-          title = 'Success';
-          body = 'Your document was restored.';
+        title = 'Success';
+        body = 'Your document was deleted.';
+        var response = JSON.parse(req.responseText);
 
-          mod.get(id, null, function ()
-          {
-            dv().fadeTo('slow', 1);
-            shimi.indexui.get(skey, sid);
-          });
-          shimi.flash(title, body).highlight();
-        }
-        else if (req.status === 409)
-        {
-          body = JSON.parse(req.responseText);
-          title = req.statusText;
+        store(restoreButton).put('document-rev', response.rev);
 
-          shimi.flash(title, body.message).error();
-        }
-        else if (req.status === 404)
-        {
-          body = 'Document was erased and cannot be restored.';
-          title = req.statusText;
+        $('#document-delete-button').hide();
+        $('#document-edit-button').hide();
+        restoreButton.show();
+        dv().fadeTo('slow', 0.5);
 
-          shimi.flash(title, body).error();
-        }
+        indexui.get(skey, sid);
+        flash.highlight(title, body);
       }
-    });
-
-    return mod;
-  };
-
-  mod.del = function (id, rev)
-  {
-    var url = './documents/' + id + '?rev=' + rev;
-    var restoreButton = $('#document-restore-button');
-    var skey = $('#first-index-element').attr('data-first-key');
-    var sid = $('#first-index-element').attr('data-first-id');
-    var body;
-    var title;
-
-    $.ajax(
-    {
-      type: 'DELETE',
-      url: url,
-      dataType: 'json',
-      contentType: 'application/json',
-      complete: function (req, status)
+      else if (req.status === 409)
       {
-        if (req.status === 200)
-        {
-          title = 'Success';
-          body = 'Your document was deleted.';
-          var response = JSON.parse(req.responseText);
+        body = JSON.parse(req.responseText);
+        title = req.statusText;
 
-          shimi.store(restoreButton).put('document-rev', response.rev);
-
-          $('#document-delete-button').hide();
-          $('#document-edit-button').hide();
-          restoreButton.show();
-          dv().fadeTo('slow', 0.5);
-
-          shimi.indexui.get(skey, sid);
-          shimi.flash(title, body).highlight();
-        }
-        else if (req.status === 409)
-        {
-          body = JSON.parse(req.responseText);
-          title = req.statusText;
-
-          shimi.flash(title, body.message).error();
-        }
-        else if (req.status === 404)
-        {
-          body = 'Document appears to have been deleted already.';
-          title = req.statusText;
-
-          shimi.flash(title, body).error();
-        }
+        flash.error(title, body.message);
       }
-    });
+      else if (req.status === 404)
+      {
+        body = 'Document appears to have been deleted already.';
+        title = req.statusText;
 
-    return mod;
-  };
-
-  mod.confirmIt = function (callback)
-  {
-    if (window.confirm('Are you sure?'))
-    {
-      var s = shimi.store(viewInfo());
-      var id = s.d('document');
-      var rev = s.d('rev');
-
-      callback(id, rev);
+        flash.error(title, body);
+      }
     }
+  });
 
-    return mod;
-  };
+  return true;
+};
 
-  mod.edit = function ()
+// Confirm an action.
+var confirmIt = function (callback)
+{
+  if (window.confirm('Are you sure?'))
   {
-    shimi.editui.resetFields();
-    if ($('#document-view-tree').hasClass('oldrev'))
-    {
-      $('#save-document-button').addClass('oldrev');
-    }
-    else
-    {
-      $('#save-document-button').removeClass('oldrev');
-    }
-    shimi.fieldsets.fillFieldsets();
-
-    return mod;
-  };
-
-  mod.confirmDelete = function ()
-  {
-    var s = shimi.store(viewInfo());
+    var s = store(viewInfo());
     var id = s.d('document');
     var rev = s.d('rev');
-    return mod.confirmIt(function ()
-    {
-      mod.del(id, rev);
-    });
-  };
 
-  mod.confirmRestore = function ()
+    callback(id, rev);
+  }
+
+  return true;
+};
+
+// Move the document to the editor.
+var edit = function ()
+{
+  editui.resetFields();
+  if ($('#document-view-tree').hasClass('oldrev'))
   {
-    var s = shimi.store(viewInfo());
-    var id = s.d('document');
-    var rev = s.d('rev');
-    return mod.confirmIt(function ()
-    {
-      mod.restore(id, rev);
-    });
-  };
-
-  mod.collapseToggle = function (target)
+    $('#save-document-button').addClass('oldrev');
+  }
+  else
   {
-    $(target).parent('li').toggleClass('collapsed');
+    $('#save-document-button').removeClass('oldrev');
+  }
+  fieldsets.fillFieldsets();
 
-    return mod;
-  };
+  return true;
+};
 
-  mod.fetchRevision = function (target)
+// Ask for confirmation on deletion.
+var confirmDelete = function ()
+{
+  var s = store(viewInfo());
+  var id = s.d('document');
+  var rev = s.d('rev');
+  return confirmIt(function ()
   {
-    var s = shimi.store($(target));
-    var id = s.d('document');
-    var oldrev = s.d('oldrev');
+    del(id, rev);
+  });
+};
 
-    $('.revision-link').removeClass('selected-revision');
-    $(target).addClass('selected-revision');
+// Ask for confirmation on restoration.
+var confirmRestore = function ()
+{
+  var s = store(viewInfo());
+  var id = s.d('document');
+  var rev = s.d('rev');
+  return confirmIt(function ()
+  {
+    restore(id, rev);
+  });
+};
 
-    mod.get(id, oldrev);
+// Expand and collapse elements of the view tree.
+var collapseToggle = function (target)
+{
+  $(target).parent('li').toggleClass('collapsed');
 
-    return mod;
-  };
+  return true;
+};
 
-  return mod;
-})();
+// Get a previous revision.
+var fetchRevision = function (target)
+{
+  var s = store($(target));
+  var id = s.d('document');
+  var oldrev = s.d('oldrev');
+
+  $('.revision-link').removeClass('selected-revision');
+  $(target).addClass('selected-revision');
+
+  get(id, oldrev);
+
+  return true;
+};
+
+exports(formatTimestamps);
+exports(get);
+exports(restore);
+exports(del);
+exports(confirmIt);
+exports(edit);
+exports(confirmDelete);
+exports(confirmRestore);
+exports(collapseToggle);
+exports(fetchRevision);
