212742ed8bdeeff2b8fb194fcd5e7443dac88a01
Noah Diewald <noah@diewald.me>
2013-09-11 01:22:07 -0500

    commonjs conversion #1889

--- priv/src/client/projects/projectui.js
+++ priv/src/client/projects/projectui.js
@@ -1,115 +1,136 @@
-shimi.projectui = (function ()
+// # The project manager
+//
+// *Implicit depends:* DOM, JQuery, JQuery UI
+//
+// Interface for working with projects.
+
+// Variable Definitions
+
+var form = require('../form.js');
+
+// Internal functions
+
+// Delete the project with the given ID.
+var deleteProject = function (id)
 {
-  'use strict';
+  'user strict';
 
-  var mod = {};
-
-  var deleteProject = function (id)
+  if (window.confirm('Are you sure? This is permanent.'))
   {
-    if (window.confirm('Are you sure? This is permanent.'))
+    $.ajax(
     {
-      $.ajax(
+      type: 'DELETE',
+      url: '/projects/' + id,
+      dataType: 'json',
+      contentType: 'application/json',
+      complete: function (req, status)
       {
-        type: 'DELETE',
-        url: '/projects/' + id,
-        dataType: 'json',
-        contentType: 'application/json',
-        complete: function (req, status)
+        if (req.status === 204)
         {
-          if (req.status === 204)
-          {
-            mod.init();
-          }
-          else
-          {
-            window.alert('An error occurred' + req.status);
-          }
+          mod.init();
         }
-      });
-    }
-  };
+        else
+        {
+          window.alert('An error occurred' + req.status);
+        }
+      }
+    });
+  }
+};
 
-  mod.add = function ()
+// Exported functions
+
+// Add a project.
+var add = function ()
+{
+  'user strict';
+
+  var projectName = $('#project-name');
+  var projectDescription = $('#project-description');
+  var tips = $('.validate-tips');
+  var allFields = $([]).add(projectName).add(projectDescription);
+
+  var dialog = $('#add-dialog').dialog(
   {
-    var projectName = $('#project-name');
-    var projectDescription = $('#project-description');
-    var tips = $('.validate-tips');
-    var allFields = $([]).add(projectName).add(projectDescription);
-
-    var dialog = $('#add-dialog').dialog(
+    autoOpen: false,
+    modal: true,
+    buttons:
     {
-      autoOpen: false,
-      modal: true,
-      buttons:
+      'Add project': function ()
       {
-        'Add project': function ()
+        allFields.removeClass('ui-state-error');
+        $('.validation-error-message').remove();
+
+        var checkResult = form.checkLength(projectName, 'project name', 1, 50, tips);
+
+        if (checkResult)
         {
-          allFields.removeClass('ui-state-error');
-          $('.validation-error-message').remove();
-
-          var checkResult = shimi.form.checkLength(projectName, 'project name', 1, 50, tips);
-
-          if (checkResult)
+          $.ajax(
           {
-            $.ajax(
+            type: 'POST',
+            url: 'projects/index',
+            dataType: 'json',
+            contentType: 'application/json',
+            processData: false,
+            data: JSON.stringify(
             {
-              type: 'POST',
-              url: 'projects/index',
-              dataType: 'json',
-              contentType: 'application/json',
-              processData: false,
-              data: JSON.stringify(
+              name: projectName.val(),
+              description: projectDescription.val()
+            }),
+            complete: function (req, status)
+            {
+              if (req.status === 201)
               {
-                name: projectName.val(),
-                description: projectDescription.val()
-              }),
-              complete: function (req, status)
-              {
-                if (req.status === 201)
-                {
-                  mod.init();
-                }
-                else
-                {
-                  window.alert('An error occurred ' + req.status);
-                }
+                mod.init();
               }
-            });
-            $(this).dialog('close');
-          }
-        },
-        'Cancel': function ()
-        {
+              else
+              {
+                window.alert('An error occurred ' + req.status);
+              }
+            }
+          });
           $(this).dialog('close');
         }
       },
-      close: function ()
+      'Cancel': function ()
       {
-        allFields.val('').removeClass('ui-state-error');
+        $(this).dialog('close');
       }
-    });
-
-    return dialog;
-  };
-
-  mod.del = function (target)
-  {
-    var id = $(target).attr('id');
-    deleteProject(id);
-
-    return mod;
-  };
-
-  mod.init = function ()
-  {
-    var url = '/projects/index';
-
-    $.get(url, function (projects)
+    },
+    close: function ()
     {
-      $('tbody').empty();
-      $('tbody').html(projects);
-    });
-  };
+      allFields.val('').removeClass('ui-state-error');
+    }
+  });
+
+  return dialog;
+};
+
+// Add a project.
+var del = function (target)
+{
+  'user strict';
+
+  var id = $(target).attr('id');
+  deleteProject(id);
 
   return mod;
-})();
+};
+
+// Initialize the interface.
+var init = function ()
+{
+  'user strict';
+
+  var url = '/projects/index';
+
+  $.get(url, function (projects)
+  {
+    $('tbody').empty();
+    $('tbody').html(projects);
+  });
+};
+
+exports(add);
+exports(del);
+exports(init);
