79fa5a1460e29e8d40b65d7988ba78f5f3e62a99
Noah Diewald <noah@diewald.me>
2013-10-04 12:41:50 -0500

    complex transformations possible #1971

--- priv/test/client/recurse_test.js
+++ priv/test/client/recurse_test.js
@@ -1,3 +1,4 @@
+var console = require('console');
 var should = require('chai').should();
 var recurse = require('../../src/client/recurse.js');
 
@@ -5,11 +6,61 @@ describe('Tail recursion', function ()
 {
   'use strict';
 
-  describe('when recursing an object', function ()
+  describe('when recursing a nested array', function ()
+  {
+    it('should return the array items in the correct order', function ()
+    {
+      var nestedArray = [1,2,[3,[4,5],6],7,[8,[9,[10,11,[12,13]]]],14,15,16];
+      var unnested = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16';
+      var walk = function (a)
+      {
+        var walk1 = function(h, t, acc, id)
+        {
+          if (h === undefined && t.length === 0)
+          {
+            return id.r(acc);
+          }
+          else if (typeof h === 'number')
+          {
+            return walk1.r(t[0], t.slice(1), acc.concat(h), id);
+          }
+
+          return walk1.r(h[0], h.slice(1).concat(t), acc, id);
+        };
+
+        return walk1.t(a[0], a.slice(1), [], recurse.identity);
+      };
+
+      walk(nestedArray).join(',').should.equal(unnested);
+    });
+  });
+  describe('when computing factorial', function ()
+  {
+    it('should complete', function ()
+    {
+      var factorial = function (a)
+      {
+        var factorial1 = function (rem, acc, id)
+        {
+          if (rem === 0)
+          {
+            return id.r(acc);
+          }
+
+          return factorial1.r(rem - 1, rem * acc, id);
+        };
+
+        return factorial1.t(a, 1, recurse.identity);
+      };
+
+      factorial(20).should.equal(2432902008176640000);
+    });
+  });
+  describe('when recursing a simple object', function ()
   {
     it('should return a transformed object', function ()
     {
-      var origObj = JSON.stringify({
+      var origObj1 = JSON.stringify({
         cat: 'sat',
         mat: {coat: 'cap'}
       });
@@ -111,7 +162,7 @@ describe('Tail recursion', function ()
 
           if (rest.length !== 0)
           {
-            return transform_.r(result[0], rest.slice(1), accObj, id);
+            return transform_.r(rest[0], rest.slice(1), accObj, id);
           }
           else
           {
@@ -122,58 +173,104 @@ describe('Tail recursion', function ()
         return transform_.t({object: o, parent: start, key: 'fields'}, [], start, recurse.identity);
       };
 
-      JSON.stringify(transform1(JSON.parse(origObj))).should.be.equal(transObj1);
-      JSON.stringify(transform2(JSON.parse(origObj))).should.be.equal(transObj2);
+      JSON.stringify(transform1(JSON.parse(origObj1))).should.be.equal(transObj1);
+      JSON.stringify(transform2(JSON.parse(origObj1))).should.be.equal(transObj2);
     });
   });
-  describe('when recursing a nested array', function ()
+  describe('when recursing a complex object', function ()
   {
-    it('should return the array items in the correct order', function ()
+    it('should return a transformed object', function ()
     {
-      var nestedArray = [1,2,[3,[4,5],6],7,[8,[9,[10,11,[12,13]]]],14,15,16];
-      var unnested = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16';
-      var walk = function (a)
+      var origObj1 = JSON.stringify({
+        cat: 'sat',
+        mat: {coat: 'cap'},
+        lap: {hide: [1,2,3]},
+        snif: [4,5,{b: 'right', back: ['o','k','?']}]
+      });
+      var transObj1 = JSON.stringify({
+        fields: [
+          {key: 'cat', index: false, string: true, number: false, array: false, object: false, value: 'sat'},
+          {key: 'mat', index: false, string: false, number: false, array: false, object: true, value: [
+            {key: 'coat', index: false, string: true, number: false, array: false, object: false, value: 'cap'}
+          ]},
+          {key: 'lap', index: false, string: false, number: false, array: false, object: true, value: [
+            {key: 'hide', index: false, string: false, number: false, array: true, object: false, value: [
+              {key: false, index: 0, string: false, number: true, array: false, object: false, value: 1},
+              {key: false, index: 1, string: false, number: true, array: false, object: false, value: 2},
+              {key: false, index: 2, string: false, number: true, array: false, object: false, value: 3}
+            ]}
+          ]},
+          {key: 'snif', index: false, string: false, number: false, array: true, object: false, value: [
+            {key: false, index: 0, string: false, number: true, array: false, object: false, value: 4},
+            {key: false, index: 1, string: false, number: true, array: false, object: false, value: 5},
+            {key: false, index: 2, string: false, number: false, array: false, object: true, value: [
+              {key: 'b', index: false, string: true, number: false, array: false, object: false, value: 'right'},
+              {key: 'back', index: false, string: false, number: false, array: true, object: false, value: [
+                {key: false, index: 0, string: true, number: false, array: false, object: false, value: 'o'},
+                {key: false, index: 1, string: true, number: false, array: false, object: false, value: 'k'},
+                {key: false, index: 2, string: true, number: false, array: false, object: false, value: '?'}
+              ]}
+            ]}
+          ]}
+        ]
+      });
+
+      var getKeyVals = function (o)
       {
-        var walk1 = function(h, t, acc, id)
+        return Object.keys(o).map(function (k)
         {
-          if (h === undefined && t.length === 0)
-          {
-            return id.r(acc);
-          }
-          else if (typeof h === 'number')
-          {
-            return walk1.r(t[0], t.slice(1), acc.concat(h), id);
-          }
+          var val = o[k];
 
-          return walk1.r(h[0], h.slice(1).concat(t), acc, id);
-        };
-
-        return walk1.t(a[0], a.slice(1), [], recurse.identity);
+          return {
+            key: (o instanceof Array) ? false : k,
+            index: (o instanceof Array) ? k * 1 : false,
+            string: (typeof val === 'string'),
+            number: (typeof val === 'number'),
+            array: (val instanceof Array),
+            object: ((val instanceof Object) && !(val instanceof Array)),
+            value: val
+          };
+        });
       };
 
-      walk(nestedArray).join(',').should.equal(unnested);
-    });
-  });
-  describe('when computing factorial', function ()
-  {
-    it('should complete', function ()
-    {
-      var factorial = function (a)
+      var transform1 = function (o)
       {
-        var factorial1 = function (rem, acc, id)
-        {
-          if (rem === 0)
-          {
-            return id.r(acc);
-          }
+        var start = {fields: []};
 
-          return factorial1.r(rem - 1, rem * acc, id);
+        var transform_ = function (o, rest, accObj, id)
+        {
+          var result;
+          var keyVals = getKeyVals(o.object);
+
+          result = keyVals.reduce(function (acc, x)
+          {
+            if (x.object || x.array)
+            {
+              return acc.concat({object: x.value, key: 'value', parent: x});
+            }
+            else
+            {
+              return acc;
+            }
+          }, []);
+
+          rest = rest.concat(result);
+          o.parent[o.key] = keyVals;
+
+          if (rest.length !== 0)
+          {
+            return transform_.r(rest[0], rest.slice(1), accObj, id);
+          }
+          else
+          {
+            return id.r(accObj);
+          }
         };
 
-        return factorial1.t(a, 1, recurse.identity);
+        return transform_.t({object: o, parent: start, key: 'fields'}, [], start, recurse.identity);
       };
 
-      factorial(20).should.equal(2432902008176640000);
+      JSON.stringify(transform1(JSON.parse(origObj1))).should.be.equal(transObj1);
     });
   });
 });
