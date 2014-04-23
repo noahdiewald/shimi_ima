1efad7b49ab5003e57618b22090353c63a48d033
Noah Diewald <noah@diewald.me>
2014-04-23 16:33:12 -0500

    moving tests around

--- priv/test/client/formalize_test.js
+++ priv/test/client/formalize_test.js
@@ -12,22 +12,6 @@ var testCase = function (fun, arg) {
 describe('Converting JSON to an HTML form', function () {
   'use strict';
 
-  describe('when provided no arguments', function () {
-    it('should raise an exception', function () {
-      var myTestCase = function () {
-        return formalize.toForm();
-      };
-
-      myTestCase.should.Throw(/invalid JSON: undefined/);
-    });
-  });
-  describe('when provided invalid type of argument', function () {
-    it('should raise an exception', function () {
-      testCase(formalize.toForm, '"string"').should.Throw(/cannot build AST from: string/);
-      testCase(formalize.toForm, '[]').should.Throw(/cannot build AST from: array/);
-      testCase(formalize.toForm, '1').should.Throw(/cannot build AST from: number/);
-    });
-  });
   describe('when provided null', function () {
     it('should return an empty form', function () {
       formalize.toForm('null').should.match(/^\s*<form>\s*<\/form>\s*$/);
--- priv/test/server/json_parse.js
+++ priv/test/server/json_parse.js
@@ -20,4 +20,20 @@ describe('Converting JSON to an AST', function () {
       testCase(json_parse.tryParseJSON, null).should.Throw(/invalid JSON: null/);
     });
   });
+  describe('when provided no arguments', function () {
+    it('should raise an exception', function () {
+      var myTestCase = function () {
+        return json_parse.tryParseJSON();
+      };
+
+      myTestCase.should.Throw(/invalid JSON: undefined/);
+    });
+  });
+  describe('when provided invalid type of argument', function () {
+    it('should raise an exception', function () {
+      testCase(json_parse.validate, 'string').should.Throw(/cannot build AST from: string/);
+      testCase(json_parse.validate, []).should.Throw(/cannot build AST from: array/);
+      testCase(json_parse.validate, 1).should.Throw(/cannot build AST from: number/);
+    });
+  });
 });
